fa239a429d2098a854cb8e5f1b8abc3065d3c59d
Justin Stevenson Theodorus <justin.theodorus@gmail.com>
2026-06-23 16:12:25 +0800

    Add: kth-largest-integer-in-a-stream - submission-1

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-1.py
@@ -0,0 +1,28 @@
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.k = k
+        self.heap = []
+        for num in nums:
+            heapq.heappush(self.heap, num)
+            if len(self.heap) > k:
+                heapq.heappop(self.heap)
+
+    def add(self, val: int) -> int:
+        heapq.heappush(self.heap, val)
+        if len(self.heap) > self.k:
+            heapq.heappop(self.heap)
+        
+        return self.heap[0]
+
+
+
+"""
+[2,3,4,4,5,5,8,9,10]
+
+store a min-heap of size k
+
+if size > k, pop
+
+"""
+        
