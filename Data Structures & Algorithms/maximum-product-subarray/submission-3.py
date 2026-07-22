e0be7480ec06ae434a50183ac714a1b60cf60ad2
Justin Stevenson Theodorus <justin.theodorus@gmail.com>
2026-07-22 14:06:44 +0800

    Add: maximum-product-subarray - submission-3

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-3.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        res = nums[0]
+        curMin, curMax = 1, 1
+
+        for num in nums:
+            tmp = curMax * num
+            curMax = max(num * curMax, num * curMin, num)
+            curMin = min(tmp, num * curMin, num)
+            res = max(res, curMax)
+        return res
+
+"""
+a running minimum can suddenly become max if the current element is negative
+
+Track min and max while iterating
+"""
