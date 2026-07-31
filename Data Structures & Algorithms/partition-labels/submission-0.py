405590bbfdee66d0b0d472f18ed255b36e381c3c
Justin Stevenson Theodorus <justin.theodorus@gmail.com>
2026-07-31 14:15:10 +0800

    Add: partition-labels - submission-0

---
+++ Data Structures & Algorithms/partition-labels/submission-0.py
@@ -0,0 +1,25 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        counter = Counter(s)
+        print(counter)
+
+        res = []
+        window = set()
+
+        l = 0
+        for r in range(len(s)):
+            if s[r] not in window:
+                window.add(s[r])
+            counter[s[r]] -= 1
+            if counter[s[r]] == 0:
+                window.remove(s[r])
+            
+            if len(window) == 0:
+                res.append(r - l + 1)
+                l = r + 1
+        return res
+
+
+"""
+Append everytime a count reaches 0
+"""
