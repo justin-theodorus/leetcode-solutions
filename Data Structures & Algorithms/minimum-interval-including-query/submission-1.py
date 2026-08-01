128e647ba8d826da4cc37038b97c3e1f2b0aa02b
Justin Stevenson Theodorus <justin.theodorus@gmail.com>
2026-08-01 11:48:54 +0800

    Add: minimum-interval-including-query - submission-1

---
+++ Data Structures & Algorithms/minimum-interval-including-query/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minInterval(self, intervals: List[List[int]], queries: List[int]) -> List[int]:
+        answer = {}
+
+        intervals.sort(key = lambda x: x[1] - x[0] + 1)
+
+        for interval in intervals:
+            for i in range(interval[0], interval[1] + 1):
+                if i not in answer:
+                    answer[i] = interval[1] - interval[0] + 1
+                
+        
+        res = []
+        for query in queries:
+            if query not in answer:
+                res.append(-1)
+            else:
+                res.append(answer[query])
+        return res
