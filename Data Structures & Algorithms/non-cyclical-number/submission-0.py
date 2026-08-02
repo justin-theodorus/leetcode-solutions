9368a28d835ee80d34e15d35926a57ba656f3332
Justin Stevenson Theodorus <justin.theodorus@gmail.com>
2026-08-02 15:08:57 +0800

    Add: non-cyclical-number - submission-0

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-0.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        def getSum(num):
+            total = 0
+            while num > 0:
+                total += ((num % 10) ** 2)
+                num //= 10
+            
+            return total
+        
+        seen = set()
+        target = set([1, 10, 100, 1000, 10000])
+        while n not in seen or n not in target:
+            if n in seen:
+                return False
+            if n in target:
+                return True
+            
+            seen.add(n)
+            n = getSum(n)
+            
